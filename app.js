b94e4e433696dff778b76e460710a974947120a0
akshit agarwal <akshitagarwal10505@gmail.com>
2020-08-16 14:07:19 +0530

    added all the database files1

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var express    = require('express')
-var mongoose   = require('mongoose')
-var Comment    =require('/models/comment')
-var app        = express();
+var express          = require('express')
+var mongoose         = require('mongoose')
+var Comment          = require('/models/comment')
+var Admin            = require('/models/admin')
+var Class            = require('/models/class')
+var emotinal_skills  = require('/models/emotional_skills')
+var Social_skills    = require('/models/social_skills')
+var Students         = require('/models/students')
+var Teachers         = require('/models/teachers')
+var app              = express();
 app.use(express.static("public"));
 
 mongoose.set("useUnifiedTopology",true);
 mongoose.set('useFindAndModify', false);
-
-Comment.insert({"Skill:"emotional","text":""})
 app.listen(3000,function(){
     console.log("server has started")
   });
